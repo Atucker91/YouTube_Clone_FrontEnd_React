01504d8dd9013dbdf7b0860754a61baed2a41a98
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-10-19 19:20:21 -0500

    added search results list from search bar input and video player that plays video when video from search list is selected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import SearchResultsList from './SearchResultsList/SearchResultsList';
+import VideoPlayer from './VideoPlayer/VideoPlayer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      selectedVideo: [],
+      videoSearchResults: [],
+      allComments: [],
+      replies: []
+     }
+  }
+
+  componentDidMount(){
+
+  }
+
+  setSelectedVideo = async (video) => {
+    this.setState({
+      selectedVideo: video
+    })
+    console.log(this.state.selectedVideo)
+  }
+
+  getSearchResults = async (searchInput) => {
+    let response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?q=${searchInput}&key=AIzaSyAOyG4Z4cTTK9TQEELOis9CYRdWmDSjq-0&maxResults=10&part=snippet`);
+    this.setState({
+      videoSearchResults: response.data.items
+    })
+    console.log(this.state.videoSearchResults)
+  }
+
+
+
+  render() { 
+    return ( 
+      <div className='row'>
+        <div className='col-xl-8'>
+          <SearchBar handleSearchSubmit={this.getSearchResults}/>
+          <VideoPlayer selectedVideo={this.state.selectedVideo} />
+        </div>
+        <div className='col-xl-4'>
+          <SearchResultsList  videoSearchResults={this.state.videoSearchResults} setSelectedVideo={this.setSelectedVideo}/>
+        </div>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const VideoPlayer = (props) => {
+    console.log(props.selectedVideo)
+    if (!props.selectedVideo || props.selectedVideo.length < 1) {
+        return <div>
+           
+           <p>
+           No Video Selected yet
+           </p>
+        </div>;
+      }
+    
+    const videoLink = `https://www.youtube.com/embed/${props.selectedVideo.id.videoId}`
+    const videoTitle = `${props.selectedVideo.snippet.title}`
+    const videoDescription = `${props.selectedVideo.snippet.description}`
+
+
+    return ( 
+        <div>
+            <iframe title={videoTitle} src={videoLink} />
+            <h3>{videoTitle}</h3>
+            <h3>{videoDescription}</h3>
+           
+        </div>
+    )
+}
+ 
+export default VideoPlayer;
